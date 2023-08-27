2108f6b12751fcc3303a3cc42760534a8c1587f8
Esther Silva <141463785+sthellar@users.noreply.github.com>
2023-08-27 11:25:32 -0300

    adicionar o exercício Pastorzinho.

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Dó, ré, mi, fá, fá, fá,
+Console.Write("Dó, "); Console.Beep(1320, 200);
+Console.Write("ré, "); Console.Beep(1485, 200);
+Console.Write("mi, "); Console.Beep(1650, 200);
+Console.Write("fá, "); Console.Beep(1759, 200);
+Thread.Sleep(500);
+Console.Write("fá, "); Console.Beep(1759, 200);
+Console.Write("fá,"); Console.Beep(1759, 200);
+Thread.Sleep(1000);
+
+Console.WriteLine();
+
+// Dó, ré, dó, ré, ré, ré,
+Console.Write("Dó, "); Console.Beep(1330, 200);
+Console.Write("ré, "); Console.Beep(1490, 200);
+Console.Write("dó, "); Console.Beep(1330, 200);
+Console.Write("ré, "); Console.Beep(1480, 200);
+Thread.Sleep(500);
+Console.Write("ré, "); Console.Beep(1485, 200);
+Console.Write("ré,"); Console.Beep(1485, 200);
+Thread.Sleep(1000);
+
+Console.WriteLine();
+
+// Dó, sol, fá, mi, mi, mi,
+Console.Write("Dó, "); Console.Beep(1330, 200);
+Console.Write("sol, "); Console.Beep(1495, 200);
+Console.Write("fá, "); Console.Beep(1770, 200);
+Console.Write("mi, "); Console.Beep(1660, 200);
+Thread.Sleep(500);
+Console.Write("mi, "); Console.Beep(1650, 200);
+Console.Write("mi,"); Console.Beep(1650, 200);
+Thread.Sleep(1000);
+
+Console.WriteLine();
+
+// Dó, ré, mi, fá, fá, fá.
+Console.Write("Dó, "); Console.Beep(1330, 200);
+Console.Write("ré, "); Console.Beep(1495, 200);
+Console.Write("mi, "); Console.Beep(1660, 200);
+Console.Write("fá, "); Console.Beep(1770, 200);
+Thread.Sleep(500);
+Console.Write("fá, "); Console.Beep(1750, 200);
+Console.Write("fá."); Console.Beep(1750, 200);
+Thread.Sleep(1000); 
